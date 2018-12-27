9119b9be23072b24ce993a115fd199977f8ecfa3
samuelxu <samuelgxu@gmail.com>
2018-12-27 08:56:34 +0800

    basic fetch from API

--- App.js
+++ App.js
@@ -1,40 +1,61 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Constants } from 'expo';
-
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Change code in the editor and watch it change on your phone! Save to get a shareable url.
-        </Text>
-        <Card>
-          <AssetExample />
-        </Card>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            dataSource: null,
+        }
+    }
+    componentDidMount () {
+        return fetch('https://swapi.co/api/planets/?format=json')
+            .then ( (response) => response.json() )
+            .then( (responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson.results,
+                })
+            })
+        .catch((error) => {
+            console.log(error)
+        });
+    }
+    render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.container}>
+                    <ActivityIndicator />
+                </View>
+            )
+        } else {
+            let results = this.state.dataSource.map((val, key) => {
+                return <View key={key} style={styles.item}><Text>{val.name + "\n" + val.diameter}</Text></View>
+            });
+            return (
+                <View style={styles.container}>
+                    
+                    {results}
+                </View>
+            );
+        }
+    }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#00CCFF',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    item: {
+        flex: 1,
+        alignSelf: 'stretch',
+        margin: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: '#eee'
+    }
 });
