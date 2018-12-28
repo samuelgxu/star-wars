623b10472ee608a9e2c6ab8e21fb10dc4a54a7b2
samuelxu <samuelgxu@gmail.com>
2018-12-28 12:04:49 +0800

    Finished App.JS functionality

--- App.js
+++ App.js
@@ -1,55 +1,146 @@
-import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, Text, FlatList, ActivityIndicator } from "react-native";
+import { List, ListItem, SearchBar } from "react-native-elements";
+import Collapsible from 'react-native-collapsible';
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            dataSource: null,
-        }
-    }
-    componentDidMount () {
-        return fetch('https://swapi.co/api/planets/?format=json')
-            .then ( (response) => response.json() )
-            .then( (responseJson) => {
-                this.setState({
-                    isLoading: false,
-                    dataSource: responseJson.results,
-                })
-            })
-        .catch((error) => {
-            console.log(error)
+class FlatListDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false
+    };
+  }
+  
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `https://swapi.co/api/planets/?format=json`;
+    this.setState({ loading: true });
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false
         });
-    }
-    render() {
-        if (this.state.isLoading) {
-            return (
-                <View style={styles.container}>
-                    <ActivityIndicator />
-                </View>
-            )
-        } else {
-            let results = this.state.dataSource.map((val, key) => {
-                return <View key={key} style={styles.item}><Text>{val.name + "\n" + val.diameter}</Text></View>
-            });
-            return (
-                <View style={styles.container}>
-                    
-                    {results}
-                </View>
-            );
-        }
-    }
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
+  handleRefresh = () => {
+    this.setState(
+      {
+        page: 1,
+        seed: this.state.seed + 1,
+        refreshing: true
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+  };
+
+  handleLoadMore = () => {
+    this.setState(
+      {
+        page: this.state.page + 1
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "0%"
+        }}
+      />
+    );
+  };
+
+  renderHeader = () => {
+    return <Text style={styles.title}>The Planets of Star Wars</Text>;
+  };
+
+  renderFooter = () => {
+    if (!this.state.loading) return null;
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderTopWidth: 1,
+          borderColor: "#CED0CE"
+        }}
+      >
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <>
+              <Text style={styles.item}> {item.name} </Text>
+              <Text style={styles.subtext}>Diameter: {item.diameter} </Text>
+              <Text style={styles.subtext}>Terrain: {item.terrain} </Text>
+              <Text style={styles.subtext}>Climate: {item.climate} </Text>
+              <Text style={styles.subtext}>Population: {item.population} </Text>
+            </>   
+          )}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+          ListFooterComponent={this.renderFooter}
+          onRefresh={this.handleRefresh}
+          refreshing={this.state.refreshing}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={50}
+        />
+      </List>
+    );
+  }
 }
+
+export default FlatListDemo;
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
+    item: {
+        flex: 0,
+        color: 'grey',
+        alignSelf: 'stretch',
+        margin: 10,
         alignItems: 'center',
         justifyContent: 'center',
+        borderBottomWidth: 0,
+        borderBottomColor: '#eee',
+        fontSize: 20,
+        fontWeight: 'bold',
     },
-    item: {
+    subtext: {
+        paddingLeft: 15,
         flex: 1,
         alignSelf: 'stretch',
         margin: 10,
@@ -57,5 +148,11 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderBottomWidth: 1,
         borderBottomColor: '#eee'
+    },
+    title: {
+      paddingLeft: 10,
+      fontSize: 30,
+      fontWeight: 'bold',
     }
 });
+
